40524032d3a64389bce5241d856052d9e17eb3f4
redaxmedia <info@redaxmedia.com>
2018-10-14 11:21:40 +0200

    Add MySQl support and 32/64 bit switches part3

--- node-setup.ps1
+++ node-setup.ps1
@@ -1,6 +1,16 @@
-$host.ui.RawUI.WindowTitle = 'Node Setup'
+If ((Get-WMIObject win32_operatingsystem).OSArchitecture -eq '64-bit')
+{
+	$title = 'Node Setup (64-Bit)'
+	$url = 'https://nodejs.org/dist/v10.12.0/node-v10.12.0-x64.msi'
+}
+Else
+{
+	$title = 'Node Setup (32-Bit)'
+	$url = 'https://nodejs.org/dist/v10.12.0/node-v10.12.0-x86.msi'
+}
+$host.ui.RawUI.WindowTitle = $title
 
 New-Item build -ItemType Directory -Force
-Invoke-WebRequest https://nodejs.org/dist/v10.12.0/node-v10.12.0-x64.msi -OutFile build\node-setup.msi
+Invoke-WebRequest $url -OutFile build\node-setup.msi
 Start-Process msiexec -ArgumentList '/i build\node-setup.msi /passive' -Wait
 Start-Process npm -ArgumentList 'install --global grunt-cli'
