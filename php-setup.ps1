8d393bc08aef2ab63226f813d91d6a0d389abde8
redaxmedia <info@redaxmedia.com>
2018-10-14 06:49:51 +0200

    Remove Uri

--- php-setup.ps1
+++ php-setup.ps1
@@ -3,7 +3,7 @@ $host.ui.RawUI.WindowTitle = 'PHP Setup'
 [System.Environment]::SetEnvironmentVariable('PATH', $Env:Path + ';C:\PHP', 'User')
 
 New-Item build -ItemType Directory -Force
-Invoke-WebRequest -Uri https://windows.php.net/downloads/releases/php-7.2.11-nts-Win32-VC15-x64.zip -OutFile build\php.zip
+Invoke-WebRequest https://windows.php.net/downloads/releases/php-7.2.11-nts-Win32-VC15-x64.zip -OutFile build\php.zip
 Expand-Archive build\php.zip -DestinationPath C:\PHP -Force
 Copy-Item php.ini -Destination C:\PHP\php.ini
 
