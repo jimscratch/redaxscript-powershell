38a2618fa882865c949cd81ddcde15e39f2ba240
redaxmedia <info@redaxmedia.com>
2018-10-14 11:18:25 +0200

    Add MySQl support and 32/64 bit switches part2

--- git-setup.ps1
+++ git-setup.ps1
@@ -1,6 +1,16 @@
-$host.ui.RawUI.WindowTitle = 'Git Setup'
+If ((Get-WMIObject win32_operatingsystem).OSArchitecture -eq '64-bit')
+{
+	$title = 'Git Setup (64-Bit)'
+	$url = 'https://github.com/git-for-windows/git/releases/download/v2.19.1.windows.1/Git-2.19.1-64-bit.exe'
+}
+Else
+{
+	$title = 'Git Setup (32-Bit)'
+	$url = 'https://github.com/git-for-windows/git/releases/download/v2.19.1.windows.1/Git-2.19.1-32-bit.exe'
+}
+$host.ui.RawUI.WindowTitle = $title
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 New-Item build -ItemType Directory -Force
-Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.19.1.windows.1/Git-2.19.1-64-bit.exe -OutFile build\git-setup.exe
+Invoke-WebRequest $url -OutFile build\git-setup.exe
 Start-Process build\git-setup.exe
--- php-setup.ps1
+++ php-setup.ps1
@@ -1,9 +1,18 @@
-$host.ui.RawUI.WindowTitle = 'PHP Setup'
+If ((Get-WMIObject win32_operatingsystem).OSArchitecture -eq '64-bit')
+{
+	$title = 'PHP Setup (64-Bit)'
+	$url = 'https://windows.php.net/downloads/releases/php-7.2.11-nts-Win32-VC15-x64.zip'
+}
+Else
+{
+	$title = 'PHP Setup (32-Bit)'
+	$url = 'https://windows.php.net/downloads/releases/php-7.2.11-nts-Win32-VC15-x86.zip'
+}
+$host.ui.RawUI.WindowTitle = $title
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 [System.Environment]::SetEnvironmentVariable('PATH', $Env:Path + ';C:\PHP', 'User')
 
 New-Item build -ItemType Directory -Force
-Invoke-WebRequest https://windows.php.net/downloads/releases/php-7.2.11-nts-Win32-VC15-x64.zip -OutFile build\php.zip
+Invoke-WebRequest $url -OutFile build\php.zip
 Expand-Archive build\php.zip -DestinationPath C:\PHP -Force
 Copy-Item php.ini -Destination C:\PHP\php.ini
-
