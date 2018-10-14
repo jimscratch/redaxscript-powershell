f8cab03ef35305d41904df597f55717de46e4b1a
Henry Ruhs <info@redaxmedia.com>
2018-10-14 22:01:06 +0200

    Update mysql-setup.ps1

--- mysql-setup.ps1
+++ mysql-setup.ps1
@@ -1,11 +1,11 @@
 If ((Get-WMIObject win32_operatingsystem).OSArchitecture -eq '64-bit')
 {
-	$title = 'MariaDB Setup (64-Bit)'
+	$title = 'MySQL Setup (64-Bit)'
 	$url = 'https://downloads.mariadb.org/f/mariadb-10.3.10/winx64-packages/mariadb-10.3.10-winx64.msi'
 }
 Else
 {
-	$title = 'MariaDB Setup (32-Bit)'
+	$title = 'MySQL Setup (32-Bit)'
 	$url = 'https://downloads.mariadb.org/f/mariadb-10.3.10/win32-packages/mariadb-10.3.10-win32.msi'
 }
 $host.ui.RawUI.WindowTitle = $title
