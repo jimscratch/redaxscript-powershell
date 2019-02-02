61467e54db0311a406cd79308ac34c7dd1804195
redaxmedia <info@redaxmedia.com>
2019-02-02 13:11:30 +0100

    Bump git, mysql and node

--- git-setup.ps1
+++ git-setup.ps1
@@ -1,12 +1,12 @@
 If ((Get-WMIObject win32_operatingsystem).OSArchitecture -eq '64-bit')
 {
 	$title = 'Git Setup (64-Bit)'
-	$url = 'https://github.com/git-for-windows/git/releases/download/v2.19.1.windows.1/Git-2.19.1-64-bit.exe'
+	$url = 'https://github.com/git-for-windows/git/releases/download/v2.20.1.windows.1/Git-2.20.1-64-bit.exe'
 }
 Else
 {
 	$title = 'Git Setup (32-Bit)'
-	$url = 'https://github.com/git-for-windows/git/releases/download/v2.19.1.windows.1/Git-2.19.1-32-bit.exe'
+	$url = 'https://github.com/git-for-windows/git/releases/download/v2.20.1.windows.1/Git-2.20.1-32-bit.exe'
 }
 $host.ui.RawUI.WindowTitle = $title
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
--- mysql-setup.ps1
+++ mysql-setup.ps1
@@ -1,12 +1,12 @@
 If ((Get-WMIObject win32_operatingsystem).OSArchitecture -eq '64-bit')
 {
 	$title = 'MySQL Setup (64-Bit)'
-	$url = 'https://downloads.mariadb.org/f/mariadb-10.3.10/winx64-packages/mariadb-10.3.10-winx64.msi'
+	$url = 'https://downloads.mariadb.org/f/mariadb-10.3.12/winx64-packages/mariadb-10.3.12-winx64.msi'
 }
 Else
 {
 	$title = 'MySQL Setup (32-Bit)'
-	$url = 'https://downloads.mariadb.org/f/mariadb-10.3.10/win32-packages/mariadb-10.3.10-win32.msi'
+	$url = 'https://downloads.mariadb.org/f/mariadb-10.3.12/win32-packages/mariadb-10.3.12-win32.msi'
 }
 $host.ui.RawUI.WindowTitle = $title
 
