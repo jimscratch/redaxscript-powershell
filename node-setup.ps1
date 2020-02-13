05cc9987ece17cc9a97228342bb8ffb6e61786b8
Henry Ruhs <info@redaxmedia.com>
2020-02-13 11:12:25 +0100

    Update node-setup.ps1

--- node-setup.ps1
+++ node-setup.ps1
@@ -1,12 +1,12 @@
 If ((Get-WMIObject win32_operatingsystem).OSArchitecture -eq '64-bit')
 {
 	$title = 'Node Setup (64-Bit)'
-	$url = 'https://nodejs.org/dist/v11.9.0/node-v11.9.0-x64.msi'
+	$url = 'https://nodejs.org/dist/v13.8.0/node-v13.8.0-x64.msi'
 }
 Else
 {
 	$title = 'Node Setup (32-Bit)'
-	$url = 'https://nodejs.org/dist/v11.9.0/node-v11.9.0-x86.msi'
+	$url = 'https://nodejs.org/dist/v13.8.0/node-v13.8.0-x86.msi'
 }
 $host.ui.RawUI.WindowTitle = $title
 
