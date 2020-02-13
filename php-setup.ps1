0ce5233805c48158c7e284c48d3eca54f781052e
Henry Ruhs <info@redaxmedia.com>
2020-02-13 11:16:31 +0100

    Update php-setup.ps1

--- php-setup.ps1
+++ php-setup.ps1
@@ -1,12 +1,12 @@
 If ((Get-WMIObject win32_operatingsystem).OSArchitecture -eq '64-bit')
 {
 	$title = 'PHP Setup (64-Bit)'
-	$url = 'https://windows.php.net/downloads/releases/latest/php-7.3-Win32-VC15-x64-latest.zip'
+	$url = 'https://windows.php.net/downloads/releases/latest/php-7.4-Win32-VC15-x64-latest.zip'
 }
 Else
 {
 	$title = 'PHP Setup (32-Bit)'
-	$url = 'https://windows.php.net/downloads/releases/latest/php-7.3-Win32-VC15-x86-latest.zip'
+	$url = 'https://windows.php.net/downloads/releases/latest/php-7.4-Win32-VC15-x86-latest.zip'
 }
 $host.ui.RawUI.WindowTitle = $title
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
