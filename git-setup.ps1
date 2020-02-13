6887fbeebe7a4144c40d43e3f6677805b4c9123c
Henry Ruhs <info@redaxmedia.com>
2020-02-13 11:14:54 +0100

    Update git-setup.ps1

--- git-setup.ps1
+++ git-setup.ps1
@@ -1,12 +1,12 @@
 If ((Get-WMIObject win32_operatingsystem).OSArchitecture -eq '64-bit')
 {
 	$title = 'Git Setup (64-Bit)'
-	$url = 'https://github.com/git-for-windows/git/releases/download/v2.20.1.windows.1/Git-2.20.1-64-bit.exe'
+	$url = 'https://github.com/git-for-windows/git/releases/download/v2.25.0.windows.1/Git-2.25.0-64-bit.exe'
 }
 Else
 {
 	$title = 'Git Setup (32-Bit)'
-	$url = 'https://github.com/git-for-windows/git/releases/download/v2.20.1.windows.1/Git-2.20.1-32-bit.exe'
+	$url = 'https://github.com/git-for-windows/git/releases/download/v2.25.0.windows.1/Git-2.25.0-32-bit.exe'
 }
 $host.ui.RawUI.WindowTitle = $title
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
