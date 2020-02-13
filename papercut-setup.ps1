af12a337af57761b5740c67e19f81f0e35dd72c7
Henry Ruhs <info@redaxmedia.com>
2020-02-13 11:13:35 +0100

    Update papercut-setup.ps1

--- papercut-setup.ps1
+++ papercut-setup.ps1
@@ -2,6 +2,6 @@ $host.ui.RawUI.WindowTitle = 'Papercut Setup'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 New-Item build -ItemType Directory -Force
-Invoke-WebRequest https://github.com/ChangemakerStudios/Papercut/releases/download/5.1.76/PapercutService.5.1.76.zip -Outfile build\papercut.zip
+Invoke-WebRequest https://github.com/ChangemakerStudios/Papercut/releases/download/5.5.1/PapercutService.5.5.1.zip -Outfile build\papercut.zip
 Expand-Archive build\papercut.zip -DestinationPath C:\Papercut -Force
 Start-Process C:\Papercut\Papercut.Service.exe -ArgumentList 'install --sudo'
