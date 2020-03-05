b0db344398e410a54ff382dc739a5a3940354809
Henry Ruhs <info@redaxmedia.com>
2020-03-05 20:12:57 +0100

    Update fontforge-setup.ps1

--- fontforge-setup.ps1
+++ fontforge-setup.ps1
@@ -1,7 +1,7 @@
-$host.ui.RawUI.WindowTitle = 'FontForce Setup'
+$host.ui.RawUI.WindowTitle = 'FontForge Setup'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 [System.Environment]::SetEnvironmentVariable('PATH', $Env:Path + ';C:\Program Files (x86)\FontForgeBuilds\bin', 'User')
 
 New-Item build -ItemType Directory -Force
-Invoke-WebRequest https://github.com/fontforge/fontforge/releases/download/20170731/FontForge-2017-07-31-Windows-r2.exe -OutFile build\fontforge-setup.exe
+Invoke-WebRequest https://github.com/fontforge/fontforge/releases/download/20190801/FontForge-2019-08-01-Windows.exe -OutFile build\fontforge-setup.exe
 Start-Process build\fontforge-setup.exe -ArgumentList '/SILENT'
