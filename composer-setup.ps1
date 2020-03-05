fb213f8a60272be362b516f7ed7e9993bf3229d1
Henry Ruhs <info@redaxmedia.com>
2020-03-05 17:16:49 +0100

    Update composer-setup.ps1

--- composer-setup.ps1
+++ composer-setup.ps1
@@ -3,4 +3,4 @@ $host.ui.RawUI.WindowTitle = 'Composer Setup'
 New-Item build -ItemType Directory -Force
 Invoke-WebRequest https://getcomposer.org/Composer-Setup.exe -OutFile build\composer-setup.exe
 Start-Process build\composer-setup.exe -Wait -ArgumentList '/SILENT /SUPPRESSMSGBOXES'
-Start-Process composer -ArgumentList 'global require hirak/prestissimo'
+
