5e00d0a24e1f7177c1b275fcc7e3f4d0d109997e
Henry Ruhs <info@redaxmedia.com>
2020-03-05 17:30:08 +0100

    Update papercut-setup.ps1

--- papercut-setup.ps1
+++ papercut-setup.ps1
@@ -5,3 +5,4 @@ New-Item build -ItemType Directory -Force
 Invoke-WebRequest https://github.com/ChangemakerStudios/Papercut/releases/download/5.5.1/PapercutService.5.5.1.zip -Outfile build\papercut.zip
 Expand-Archive build\papercut.zip -DestinationPath C:\Papercut -Force
 Start-Process C:\Papercut\Papercut.Service.exe -ArgumentList 'install --sudo'
+Start-Process C:\Papercut\Papercut.Service.exe -ArgumentList 'start'
