2e04fd8f4834c2d6822023ee8afc50b14ba0ddf8
Henry Ruhs <info@redaxmedia.com>
2020-03-05 17:16:56 +0100

    Update composer-setup.ps1

--- composer-setup.ps1
+++ composer-setup.ps1
@@ -3,4 +3,3 @@ $host.ui.RawUI.WindowTitle = 'Composer Setup'
 New-Item build -ItemType Directory -Force
 Invoke-WebRequest https://getcomposer.org/Composer-Setup.exe -OutFile build\composer-setup.exe
 Start-Process build\composer-setup.exe -Wait -ArgumentList '/SILENT /SUPPRESSMSGBOXES'
-
