2e6c9098f30d841d5c5c6d2f35e487b8626aa479
Henry Ruhs <info@redaxmedia.com>
2020-03-05 16:44:00 +0100

    Update fontforge-setup.ps1

--- fontforge-setup.ps1
+++ fontforge-setup.ps1
@@ -4,4 +4,4 @@ $host.ui.RawUI.WindowTitle = 'FontForce Setup'
 
 New-Item build -ItemType Directory -Force
 Invoke-WebRequest https://github.com/fontforge/fontforge/releases/download/20170731/FontForge-2017-07-31-Windows-r2.exe -OutFile build\fontforge-setup.exe
-Start-Process build\fontforge-setup.exe
+Start-Process build\fontforge-setup.exe -ArgumentList '/SILENT'
