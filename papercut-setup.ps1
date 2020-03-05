968bc6c83904c4bbe8ec698c401bfb191d593f2a
Henry Ruhs <info@redaxmedia.com>
2020-03-05 17:28:26 +0100

    Update papercut-setup.ps1

--- papercut-setup.ps1
+++ papercut-setup.ps1
@@ -4,4 +4,4 @@ $host.ui.RawUI.WindowTitle = 'Papercut Setup'
 New-Item build -ItemType Directory -Force
 Invoke-WebRequest https://github.com/ChangemakerStudios/Papercut/releases/download/5.5.1/PapercutService.5.5.1.zip -Outfile build\papercut.zip
 Expand-Archive build\papercut.zip -DestinationPath C:\Papercut -Force
-Start-Process C:\Papercut\Install-Papercut-Service.bat
+Start-Process C:\Papercut\Papercut.Service.exe -ArgumentList 'install --sudo'
