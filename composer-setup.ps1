3cc3ec669730901f51e634cf136b974179d3b648
Henry Ruhs <info@redaxmedia.com>
2020-03-06 23:58:42 +0100

    Update composer-setup.ps1

--- composer-setup.ps1
+++ composer-setup.ps1
@@ -1,5 +1,8 @@
 $host.ui.RawUI.WindowTitle = 'Composer Setup'
 
-New-Item build -ItemType Directory -Force
-Invoke-WebRequest https://getcomposer.org/Composer-Setup.exe -OutFile build\composer-setup.exe
-Start-Process build\composer-setup.exe -ArgumentList '/SILENT /SUPPRESSMSGBOXES'
+New-Item C:\build -ItemType Directory -Force
+if (!(Test-Path C:\build\fontforge-setup.exe))
+{
+	Invoke-WebRequest https://getcomposer.org/Composer-Setup.exe -OutFile C:\build\composer-setup.exe
+}
+Start-Process C:\build\composer-setup.exe -Wait -ArgumentList '/SILENT /SUPPRESSMSGBOXES'
