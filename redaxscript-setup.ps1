deab133d5130042aa0e6bd3c5815dc8d6d14fc35
Henry Ruhs <info@redaxmedia.com>
2020-03-07 00:21:03 +0100

    Update redaxscript-setup.ps1

--- redaxscript-setup.ps1
+++ redaxscript-setup.ps1
@@ -1,5 +1,5 @@
 $host.ui.RawUI.WindowTitle = 'Redaxscript Setup'
 
-Start-Process git -ArgumentList 'clone https://github.com/redaxscript/redaxscript.git' -Wait
-Start-Process npm -ArgumentList 'install' -WorkingDirectory redaxscript
-Start-Process composer -ArgumentList 'install' -WorkingDirectory redaxscript
+Start-Process git -ArgumentList 'clone https://github.com/redaxscript/redaxscript.git C:\redaxscript' -Wait
+Start-Process npm -ArgumentList 'install' -WorkingDirectory C:\redaxscript
+Start-Process composer -ArgumentList 'install' -WorkingDirectory C:\redaxscript
