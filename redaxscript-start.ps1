961a2c67f71798b1d71bf386e9b2927abc7c0fe9
Henry Ruhs <info@redaxmedia.com>
2020-03-07 00:22:32 +0100

    Update redaxscript-start.ps1

--- redaxscript-start.ps1
+++ redaxscript-start.ps1
@@ -1,4 +1,3 @@
 $host.ui.RawUI.WindowTitle = 'Redaxscript Start'
 
-Start-Process C:\Papercut\Papercut.Service.exe -ArgumentList 'start'
 Start-Process grunt -ArgumentList 'serve -O' -WorkingDirectory C:\redaxscript
