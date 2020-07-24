7f91e58ad7fd299b62b31f15f23ea5ee9077fe9f
redaxmedia <info@redaxmedia.com>
2020-07-24 22:54:42 +0200

    Improve redaxscript setup and re-add start part2

--- redaxscript-setup.ps1
+++ redaxscript-setup.ps1
@@ -3,4 +3,4 @@ $host.ui.RawUI.WindowTitle = 'Redaxscript Setup'
 Start-Process git -ArgumentList 'clone https://github.com/redaxscript/redaxscript.git C:\Redaxscript' -Wait
 Start-Process npm -ArgumentList 'install' -WorkingDirectory C:\Redaxscript
 Start-Process composer -ArgumentList 'install' -WorkingDirectory C:\Redaxscript
-Set-Location C:\Redaxscript
+
