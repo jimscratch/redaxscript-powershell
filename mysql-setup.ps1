4342ebf564aca6dc47e77f83f45efb21f787c5e0
Henry Ruhs <info@redaxmedia.com>
2021-05-10 15:17:00 +0200

    Update mysql-setup.ps1

--- mysql-setup.ps1
+++ mysql-setup.ps1
@@ -1,12 +1,12 @@
 If ((Get-CimInStance CIM_OperatingSystem).OSArchitecture -eq '64-bit')
 {
 	$title = 'MySQL Setup (64-Bit)'
-	$url = 'http://nyc2.mirrors.digitalocean.com/mariadb/mariadb-10.5.8/winx64-packages/mariadb-10.5.8-winx64.msi'
+	$url = 'https://downloads.mariadb.org/interstitial/mariadb-10.5.10/winx64-packages/mariadb-10.5.10-winx64.msi'
 }
 Else
 {
 	$title = 'MySQL Setup (32-Bit)'
-	$url = 'http://nyc2.mirrors.digitalocean.com/mariadb/mariadb-10.5.8/win32-packages/mariadb-10.5.8-win32.msi'
+	$url = 'https://downloads.mariadb.org/interstitial/mariadb-10.5.10/win32-packages/mariadb-10.5.10-win32.msi'
 }
 $host.ui.RawUI.WindowTitle = $title
 
